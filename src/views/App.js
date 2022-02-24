57a1957294e8a0baa0180867a913edff7db181e0
phucdo2601 <ngocphucdo2601@gmail.com>
2022-02-24 18:01:44 +0700

    Learn Reactjs-Component-b-01

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './example/MyComponent';
 
-function App() {
+/**
+ * Component giup chia cac ui thanh cac khoi doc lap co the tai su dung
+ * @returns 
+ */
+
+/**
+ * 2 components: class component/ function component(function, arrow)
+ * 
+ * 
+ * @returns 
+ */
+
+/**
+ * Basic function: ex: function nameFunc() {.... code ....}
+ * @returns 
+ */
+
+/**
+ * arrow function: ex: const  nameFunc =() =>{....code....}
+ * 
+ * @returns 
+ */
+
+//arrow
+const App = () => {
+  //khi return la mot ham xu ly giao dien
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +43,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent/>
       </header>
     </div>
   );
---
+++ src/views/example/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+    //Muon render ra noi dung thi can khai bao render(){return()}
+  render() {
+    return (
+      <div>Hello My Component by PhucDn!</div>
+    )
+  }
+}
+
+//export default: mac dinh tra ve file dat sau chu default, muon tra ve nhieu file thi: export {}
+export default MyComponent
