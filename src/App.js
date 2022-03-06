757938a5aac048fc6d58c631969652f036f8dcac
phucdo2601 <ngocphucdo2601@gmail.com>
2022-03-06 10:42:33 +0700

    Learn Basic react-router-dom v6 b01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import News from "./pages/News";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/news">News</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+
+      {/* tu react-router-v6, routes thay doi cho switch, giup chuyen doi cac component,
+        - TAO ra co che dinh tuyen noi bo
+      */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
