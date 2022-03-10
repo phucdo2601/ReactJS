169c7f659b77559c55085f554f54b59ffe5c0bea
phucdo2601 <ngocphucdo2601@gmail.com>
2022-03-10 21:39:50 +0700

    Learn print variable on react-hook

--- src/App.js
+++ src/App.js
@@ -32,18 +32,37 @@ import "./App.css";
  * @returns
  */
 const App = () => {
+  /**
+   * Viet code logic thi nen viet o tren return()
+   *
+   */
+
+  let name = "PhucDn";
+  let age = 22;
+  let boolean = true;
+  let obj = {
+    name: "PhucDn",
+    age: 22,
+  };
+
+  let link = "https://www.facebook.com/profile.php?id=100034519140023";
+
   return (
     <div className="App">
+      {console.log("check obj: ", obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World By PhucDN!</p>
+        <p>
+          Hello World By {name} and He is {obj.age}!
+        </p>
+        <p>{JSON.stringify(obj)}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={link}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to my PhucDn Facebook
         </a>
       </header>
     </div>
