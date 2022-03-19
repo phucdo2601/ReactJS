9cdb83bd6636c5833c836716dab8c20abc6e12c7
phucdo2601 <ngocphucdo2601@gmail.com>
2022-03-19 21:57:24 +0700

    First learn basic renders loop in reactjs-hook-b01

--- src/App.js
+++ src/App.js
@@ -53,13 +53,42 @@ const App = () => {
    */
   let [name, setName] = useState("Eric"); //[a1, b1, c1,...]
   const [address, setAddress] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      title: "test-01",
+    },
+    {
+      id: "todo2",
+      title: "test-02",
+    },
+    {
+      id: "todo3",
+      title: "test-03",
+    },
+  ]);
 
   //dinh nghia ham trong function component in react_js
 
   const handleEventClick = (event) => {
+    if (!address) {
+      alert("The input field is blank! Please fill the input");
+      return;
+    }
     //set lai gia tri trong ham cua state
-    setName(address);
-    console.log(address);
+    // setName(address);
+    // console.log(address);
+    /**
+     * hook not merge state
+     */
+    /**
+     * ...spread syntax array js
+     */
+    let newTodo = {
+      id: "abc",
+      title: address,
+    };
+    setTodos([...todos, newTodo]);
   };
 
   const handleOnChangeInput = (event) => {
@@ -74,10 +103,23 @@ const App = () => {
   return (
     <div className="App">
       {/* Chi dung <Nav></Nav> Khi va chi khi muon chuyen noi dung co comp dc goi */}
+      {/* su dung map de lap => tra ve mot array moi, khong bi anh huong den dl ma minh lap */}
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World By {name} !</p>
+
+        <div className="todos-container">
+          {todos.map((todo) => {
+            console.log(">>>check todo: ", todo);
+            return (
+              // can dat key de nang cao hieu nang reactjs, va bat su kien
+              <li className="todo-child" key={todo.id}>
+                {todo.title}
+              </li>
+            );
+          })}
+        </div>
 
         <input
           type="text"
