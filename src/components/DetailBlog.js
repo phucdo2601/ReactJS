4fe86a8b722580712d8cb139fb954ea081005576
phucdo2601 <ngocphucdo2601@gmail.com>
2022-03-22 21:06:52 +0700

    Learn ReUsing custom hook in reactjs-hook b01

--- src/components/DetailBlog.js
+++ src/components/DetailBlog.js
@@ -1,4 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
+import useCovidFetch from "../customize/covidFetch";
+import "../styles/Blog.scss";
 
 const DetailBlog = () => {
   /**
@@ -12,12 +14,32 @@ const DetailBlog = () => {
     navigate("/blogs");
   };
 
+  const {
+    data: dataBlogDetail,
+    isLoading,
+    isError,
+  } = useCovidFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false);
+
+  console.log(">>> check blog details ", dataBlogDetail);
+
   return (
     <>
       <div>
         <span onClick={() => handleBackData()}>&lt; -- Back</span>
       </div>
       <h1>Hello Detail blogs with id = {id}</h1>
+
+      <div className="blog-detail">
+        {dataBlogDetail && (
+          <>
+            <div className="title">
+              Blog Id:{id} ---{" "}
+              {isLoading === true ? "Loading data..." : dataBlogDetail.title}
+            </div>
+            <div className="content">{dataBlogDetail.body}</div>
+          </>
+        )}
+      </div>
     </>
   );
 };
