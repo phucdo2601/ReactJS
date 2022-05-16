49de5d434c10df7aee4882e93397cdbd1236e10d
phucdo2601 <ngocphucdo2601@gmail.com>
2022-05-16 22:33:03 +0700

    First learn chartjs with reactjs b01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import { useState } from "react";
+import { UserData } from "./Data";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Lost",
+        data: UserData.map((data) => data.userLost),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
+
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
     </div>
   );
 }
---
+++ src/components/PieChart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+const PieChart = ({ chartData }) => {
+  return (
+    <>
+      <div>
+        <Pie data={chartData} />
+      </div>
+    </>
+  );
+};
+
+export default PieChart;
