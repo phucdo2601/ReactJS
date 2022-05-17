9635e018dce79dc0245b394c7a4816e844b88e7e
phucdo2601 <ngocphucdo2601@gmail.com>
2022-05-17 21:03:34 +0700

    First Learn basic sheetjs with reactjs b01 in 5/2022

---
+++ src/Components/ParseExcel.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx/xlsx.mjs";
+const ParseExcel = () => {
+  const [fileName, setFileName] = useState(null);
+  const [columns, setColumns] = useState([]);
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+
+    setFileName(file.name);
+    const data = await file.arrayBuffer();
+    const workBook = XLSX.readFile(data, { sheetRows: 5 });
+
+    const worksheet = workBook.Sheets[workBook.SheetNames[0]];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+      header: 1,
+      defval: "",
+    });
+
+    setColumns(jsonData[0]);
+
+    console.log(jsonData);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Parse Excel</h1>
+        {fileName && (
+          <React.Fragment>
+            <p>
+              FileName: <span>{fileName}</span>
+            </p>
+
+            <p>
+              Column:
+              <select>
+                {columns.map((column) => (
+                  <option value={column}>{column}</option>
+                ))}
+              </select>
+            </p>
+          </React.Fragment>
+        )}
+        <input type="file" onChange={(e) => handleFileChange(e)} />
+      </div>
+    </>
+  );
+};
+
+export default ParseExcel;
