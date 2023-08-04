2fbfd2bebb71cfdc7da426d68e91f7b3f4b037af
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-08-04 09:29:42 +0700

    [Basic-TetrisGame-b01] 01-Create func random item tetris b01

--- l-terris-game-b01/src/components/Stage.jsx
+++ l-terris-game-b01/src/components/Stage.jsx
@@ -5,7 +5,15 @@ const Stage = ({stage}) => {
   return (
     <>
       <div>
-        <Cell />
+        {
+          stage.map((row) => row.map((cell, x) => {
+            return (
+              <>
+                <Cell key={x} type={cell[0]} />
+              </>
+            )
+          }))
+        }
       </div>
     </>
   )
