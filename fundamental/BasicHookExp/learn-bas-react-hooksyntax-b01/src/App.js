e7ff636fb3da6ed9a7d9070405c5c5d54b64928f
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-30 11:08:11 +0700

    [learn-bas-react-hook-syntax-2023] 03. Basi useReducer b01

--- fundamental/BasicHookExp/learn-bas-react-hooksyntax-b01/src/App.js
+++ fundamental/BasicHookExp/learn-bas-react-hooksyntax-b01/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import LearnUseStateHook from './listHooks/LearnUseStateHook'
 import LearnUseEffect from './listHooks/LearnUseEffect'
 import LearnUseContex from './listHooks/LearnUseContex'
+import LearnUseReducer from './listHooks/LearbUseReducer'
 
 const App = () => {
   return (
     <>
       {/* <LearnUseStateHook /> */}
       {/* <LearnUseEffect /> */}
-      <LearnUseContex />
+      {/* <LearnUseContex /> */}
+      <LearnUseReducer />
     </>
   )
 }
