f60032395476743c20fe4ca656de69f906338160
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-30 09:26:55 +0700

    [learn-bas-react-hook-syntax-2023] 02. Basic learn useEffect hook b01

--- fundamental/BasicHookExp/learn-bas-react-hooksyntax-b01/src/App.js
+++ fundamental/BasicHookExp/learn-bas-react-hooksyntax-b01/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import LearnUseStateHook from './listHooks/LearnUseStateHook'
+import LearnUseEffect from './listHooks/LearnUseEffect'
 
 const App = () => {
   return (
     <>
-      <LearnUseStateHook />
+      {/* <LearnUseStateHook /> */}
+      <LearnUseEffect />
     </>
   )
 }
