55d645d65bbf336e5c7110b8dc3e837bd915bdda
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-02-02 18:37:34 +0700

    [2025-react-router-v7-learn-fun-react-router-v7-b01] 03. Basic learn loader and action in react router v7 b01

--- listFunReactPro/2025/ReactRouter/v7/fun/learn-fun-react-router-v7-b01/app/routes/post.tsx
+++ listFunReactPro/2025/ReactRouter/v7/fun/learn-fun-react-router-v7-b01/app/routes/post.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Route } from "./+types/post";
+import axios from "axios";
+import { Form } from "react-router";
 
-export const loader = async ({ params }: Route.LoaderArgs) => {
+interface PostModel {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const clientLoader = async ({ params }: Route.LoaderArgs) => {
   const postId = params.postId;
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
+  );
+  const returnData: PostModel = response.data;
   return {
     postId,
+    returnData,
   };
 };
 
-export async function action() {}
+export async function clientAction({ params }: Route.LoaderArgs) {
+  await axios.delete(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+}
 
 const Post = ({ loaderData }: Route.ComponentProps) => {
   return (
@@ -17,6 +35,19 @@ const Post = ({ loaderData }: Route.ComponentProps) => {
         <p className="text-3xl font-bold underline">
           Post ID: {loaderData.postId}
         </p>
+        <p className="text-3xl font-bold underline">
+          Post UserId: {loaderData.returnData?.userId}
+        </p>
+        <p className="text-3xl font-bold underline">
+          Post Title: {loaderData.returnData?.title}
+        </p>
+        <p className="text-3xl font-bold underline">
+          Post Body: {loaderData.returnData?.body}
+        </p>
+
+        <Form method="delete">
+          <button type="submit">Delete</button>
+        </Form>
       </div>
     </>
   );
